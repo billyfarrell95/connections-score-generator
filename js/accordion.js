43ddaa5d4aff7df6a08bdb8c74e7951accc7b965
billyfarrell95 <billyfarrell95@gmail.com>
2024-03-10 03:23:23 -0500

    removed accordion

--- js/accordion.js
+++
@@ -1,9 +0,0 @@
-const infoBtn = document.getElementById('info-btn');
-const accordionTitle = document.getElementById('accordion-title');
-const accordionContent = document.getElementById('accordion-content');
-
-infoBtn.addEventListener('click', ()=> {
-    accordionTitle.classList.toggle('open');
-    accordionContent.classList.toggle('d-none');
-    accordionContent.classList.toggle('open');
-})
